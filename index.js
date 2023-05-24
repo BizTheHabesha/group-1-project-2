9febbd49fe806bf25430f5d29d5f5b47141df5d2
tylersmith2614 <tylersmith2614@gmail.com>
2023-05-23 19:47:10 -0600

    API-Football API Snippet

---
+++ index.js
@@ -0,0 +1,17 @@
+// API-Football Code Snippet with API Key
+const url = "https://api-football-v1.p.rapidapi.com/v3/timezone";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "27fce6bde4msh7acd7c7b3f1c7fap1ccc0ejsn804991608a10",
+    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+  },
+};
+
+try {
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
