a9553ebf58d0c2e9de858e7e243c78b263191031
Your Name <you@example.com>
2023-06-01 19:13:54 -0600

    added phone number to model

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,54 @@
-const loginBtn = $('#login')
+const loginBtn = $('#loginBtn')
+const signUpBtn = $('#signUpBtn')
+
+$('#signUpBtn').submit( async (e) => {
+    e.preventDefault();
+
+    const email = $('#register-email').val().trim();
+    const password = $('#register-password').val().trim();
+    const phoneNumber = $('#register-phone').val().trim();
+    
+    const signUp = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password,
+            phone,
+        })
+    });
+
+    if (signUp.ok) {
+        document.location.replace('/');
+    } else {
+        const errorData = await signUp.json();
+    }
+});
+
+// Login Button
+
+$('#loginBtn').submit( async (e) => {
+    e.preventDefault();
+
+    const email = $('#login-email').val().trim();
+    const password = $('#login-password').val().trim();
+
+    const logIn = await fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: username,
+            password
+        })
+    });
+
+    if (logIn.ok) {
+        document.location.replace('/');
+    } else {
+        const errorData = await logIn.json();
+    }
+});
