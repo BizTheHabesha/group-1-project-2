6347eba4ffe527e857353364928a4e448a69f76e
Biz <github@bisrat.dev>
2023-06-03 17:10:09 -0600

    add alert renderer

--- public/js/login.js
+++ public/js/login.js
@@ -1,54 +1,76 @@
-const loginBtn = $('#loginBtn')
-const signUpBtn = $('#signUpBtn')
+const loginBtn = $("#loginBtn");
+const signUpBtn = $("#signUpBtn");
 
-$('#signUpBtn').submit( async (e) => {
-    e.preventDefault();
+function doAlert(message, _severity) {
+	let severity;
+	switch (_severity) {
+		case "error":
+			severity = "alert-danger";
+			break;
+		case "warn":
+			severity = "alert-warning";
+			break;
+		case "success":
+			severity = "alert-success";
+			break;
+		case "info":
+			severity = "alert-primary";
+			break;
+		default:
+			severity = "alert-dark";
+			break;
+	}
+	$(".display-popup").addClass(severity).show();
+}
 
-    const email = $('#register-email').val().trim();
-    const password = $('#register-password').val().trim();
-    const phoneNumber = $('#register-phone').val().trim();
-    
-    const signUp = await fetch('/api/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email,
-            password,
-            phone,
-        })
-    });
+$("#signUpBtn").submit(async (e) => {
+	e.preventDefault();
 
-    if (signUp.ok) {
-        document.location.replace('/');
-    } else {
-        const errorData = await signUp.json();
-    }
+	const email = $("#register-email").val().trim();
+	const password = $("#register-password").val().trim();
+	const phoneNumber = $("#register-phone").val().trim();
+
+	const signUp = await fetch("/api/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			email,
+			password,
+			phone,
+		}),
+	});
+
+	if (signUp.ok) {
+		document.location.replace("/");
+	} else {
+		const errorData = await signUp.json();
+	}
 });
 
 // Login Button
 
-$('#loginBtn').submit( async (e) => {
-    e.preventDefault();
+$("#loginBtn").submit(async (e) => {
+	e.preventDefault();
 
-    const email = $('#login-email').val().trim();
-    const password = $('#login-password').val().trim();
+	const email = $("#login-email").val().trim();
+	const password = $("#login-password").val().trim();
 
-    const logIn = await fetch('/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: username,
-            password
-        })
-    });
+	const logIn = await fetch("/api/users/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			email: username,
+			password,
+		}),
+	});
 
-    if (logIn.ok) {
-        document.location.replace('/');
-    } else {
-        const errorData = await logIn.json();
-    }
+	if (logIn.ok) {
+		document.location.replace("/");
+	} else {
+		doAlert(JSON.stringify(logIn.json()), "error");
+	}
 });
