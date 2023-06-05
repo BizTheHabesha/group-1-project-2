2bcccb24992b9de1287bda7e471f3fbefaad76fd
tylersmith2614 <tylersmith2614@gmail.com>
2023-06-05 17:58:24 -0600

    soccer searches return a table of standings and recent matches

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,62 +5,61 @@ const withAuth = require("../utils/auth");
 const withoutAuth = require("../utils/woauth");
 
 router.get("/", withAuth, async (req, res) => {
-	const clog = new ClogHttp("GET /", true);
-	try {
-		clog.httpStatus(200);
-		res.render("homepage", {
-			API_KEY: process.env.API_KEY,
-			logged_in: !!req.session.logged_in,
-		});
-		clog.info("rendered");
-	} catch (err) {
-		res.render("homepage", {
-			logged_in: !!req.session.logged_in,
-			error: err.message,
-			status: 500,
-		});
-	}
+  const clog = new ClogHttp("GET /", true);
+  try {
+    clog.httpStatus(200);
+    res.render("homepage", {
+      API_KEY: process.env.API_KEY,
+      logged_in: !!req.session.logged_in,
+    });
+    clog.info("rendered");
+  } catch (err) {
+    res.render("homepage", {
+      logged_in: !!req.session.logged_in,
+      error: err.message,
+      status: 500,
+    });
+  }
 });
 
 router.get("/login", withoutAuth, async (req, res) => {
-	const clog = new ClogHttp("GET /login", true);
-	try {
-		clog.httpStatus(200);
-		res.render("login", {
-			logged_in: !!req.session.logged_in,
-			render_as_body: true,
-			custom_css: ["login"],
-		});
-		clog.info("rendered");
-	} catch (err) {
-		clog.httpStatus(500, err.message);
-		res.render("login", {
-			logged_in: !!req.session.logged_in,
-			render_as_body: true,
-			custom_css: ["login"],
-			error: err.message,
-			status: 500,
-		});
-		clog.critical(JSON.stringify(err));
-	}
+  const clog = new ClogHttp("GET /login", true);
+  try {
+    clog.httpStatus(200);
+    res.render("login", {
+      logged_in: !!req.session.logged_in,
+      render_as_body: true,
+      custom_css: ["login"],
+    });
+    clog.info("rendered");
+  } catch (err) {
+    clog.httpStatus(500, err.message);
+    res.render("login", {
+      logged_in: !!req.session.logged_in,
+      render_as_body: true,
+      custom_css: ["login"],
+      error: err.message,
+      status: 500,
+    });
+    clog.critical(JSON.stringify(err));
+  }
 });
-
 router.get("/search", withAuth, async (req, res) => {
-	const clog = new ClogHttp("GET /search");
-	try {
-		res.render("search", {
-			logged_in: !!req.session.logged_in,
-		});
-		clog.httpStatus(200);
-		clog.info("rendered");
-	} catch (err) {
-		clog.httpStatus(500, err.message);
-		res.render("search", {
-			logged_in: !!req.session.logged_in,
-			status: 500,
-			error: err.message,
-		});
-	}
+  const clog = new ClogHttp("GET /search");
+  try {
+    res.render("search", {
+      logged_in: !!req.session.logged_in,
+    });
+    clog.httpStatus(200);
+    clog.info("rendered");
+  } catch (err) {
+    clog.httpStatus(500, err.message);
+    res.render("search", {
+      logged_in: !!req.session.logged_in,
+      status: 500,
+      error: err.message,
+    });
+  }
 });
 
 module.exports = router;
